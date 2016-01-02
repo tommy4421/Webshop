2cfad42c88404a602ff93911258daf1e174a1d54
tommy4421 <tom.maljaars@gmail.com>
2016-01-02 16:04:07 +0100

    db2

--- includes/mysqli_connect_webpages.avans.nl.php
+++ includes/mysqli_connect_webpages.avans.nl.php
@@ -4,11 +4,10 @@
 // This file also establishes a connection to MySQL 
 // and selects the database.
 
-DEFINE ('servername', 'localhost');
-DEFINE ('username', 'bimivp2e4');
-DEFINE ('password', 'Welkom01');
-DEFINE ('dbname', 'avans_bimivp2e4');
-
+DEFINE ('DB_HOST', 'localhost');
+DEFINE ('DB_USER', 'bimivp2e4');
+DEFINE ('DB_PASSWORD', 'Welkom01');
+DEFINE ('DB_NAME', 'avans_bimivp2e4');
 
 // Make the connection:
 // $conn = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
