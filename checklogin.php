3accdd30b527437dec1cbaa1726abbf9b664195e
TheKevinator <kjneeter@avans.nl>
2016-01-04 00:51:13 +0100

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -31,11 +31,14 @@ $email=$_POST['email'];
 $wachtwoord=$_POST['wachtwoord'];
 
 // Maak de SQL query die onze bestellingen gaat opleveren.
-$sql = "SELECT * FROM `Klant` WHERE `Email`='$email';"; 
+$sql = "SELECT * FROM `Klant` WHERE `Email`='$email';";
+
+$query="SELECT * FROM `Klant` WHERE Email='$email' and Wachtwoord='$wachtwoord'";
+$resultaat=mysql_query($query);
 
 // Voer de query uit en sla het resultaat op 
 		// Voer de query uit en vang fouten op 
-if( !($result = mysqli_query($conn, $sql)) ) {
+if( !($result = mysqli_query($conn, $sql, $resultaat)) ) {
 	echo "<p>Geen resultaten gevonden.</p>\n";
 } else {
 	// We zoeken één klant, dus slechts één row is nodig.
