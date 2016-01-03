e619ea04c2305e1b15f59859dddfde6a0ce2dafe
TheKevinator <kjneeter@avans.nl>
2016-01-03 23:48:09 +0100

    Create checklogin.php

---
+++ checklogin.php
@@ -0,0 +1,101 @@
+<?php
+//index.php
+//startscherm van de webwinkel
+// Zet het niveau van foutmeldingen zo dat warnings niet getoond worden.
+error_reporting(E_ERROR | E_PARSE);
+$page_title = 'Tijdvooreenbox.nl ~ Login';
+include ('includes/header.html');
+// mysqli_connect.php bevat de inloggegevens voor de database.
+// Per server is er een apart inlogbestand - localhost vs. remote server
+//include ('includes/mysqli_connect_'.$_SERVER['SERVER_NAME'].'.php');
+include ('includes/mysqli_connect_localhost.php');
+
+?>
+<?php
+
+// Deze code zorgt ervoor dat de ingevulde gegevens worden gebruikt als variabele email en wachtwoord
+
+$email=$_POST['email'];
+$wachtwoord=$_POST['wachtwoord'];
+$tabel='Klant';
+
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+if (mysqli_connect_errno()) {
+	printf("<p><b>Fout: verbinding met de database mislukt.</b><br/>\n%s</p>\n", mysqli_connect_error());
+	include ('includes/footer.html');
+	exit();
+}
+
+// Deze code selecteert het ledennummer dat bij de ingevulde gebruikersnaam hoort.
+// De fetch array zorgt ervoor dat er als uitkomst van de query niet uitkomt 'resource id blabla', maar juist 
+// de letterlijke uitkomst, dus het ledennnummer.
+
+$naam1 = mysql_query("SELECT Naam FROM $tabel where Email='$email'");
+$naam2 = mysql_fetch_array($naam1);
+$naam = ($naam2['naam']);
+
+$klantid1 = mysql_query("SELECT KlantID FROM $tabel WHERE Email='$email'");
+$klantid2 = mysql_fetch_array($id1);
+$klantid = ($klantid2['KlantID']);
+
+$w8woord1 = mysql_query("SELECT Wachtwoord FROM $tabel WHERE Email='$email'");
+$w8woord2 = mysql_fetch_array($w8woord1);
+$w8woord = ($w8woord2['wachtwoord']);
+
+// Deze code is er om SQL Injections tegen te gaan. Zo kan 'vijandelijke' invoer geweigerd worden.
+$email = stripslashes($email);
+$wachtwoord = stripslashes($wachtwoord);
+$email = mysql_real_escape_string($email);
+$wachtwoord = mysql_real_escape_string($wachtwoord);
+
+// Deze code tel de rijen die er uit de query komen. Als de uitkomst precies 1 is, betekent dit dat de
+// ingevulde gebruikersnaam en wachtwoord de juiste combinatie zijn, en dus dat de gegevens kloppen
+$sql="SELECT * FROM $tabel WHERE Email='$email' and wachtwoord='$wachtwoord'";
+$result=mysql_query($sql);
+$count=mysql_num_rows($result);
+
+
+// Deze code checkt of de ingevulde gegevens kloppen, en hij checkt of de gebruiker een admin is.
+// Als de gebruiker een admin is, wordt dit opgeslagen met Session['admin'].
+// De gebruikersnaam en het lidnummer van de gebruiker worden ook in de sessie opgeslagen
+// Mocht er iets niet kloppen, dan wordt de gebruiker terug gestuurd naar het inlogscherm
+  if (mysql_num_rows($resultaat) > 0 ){
+        $admin=mysql_fetch_array($resultaat);
+
+		session_start();
+		$_SESSION['klantnr'] = $klantid;
+		$_SESSION['Naam'] = $naam;
+		$_SESSION['Email'] = $email;
+		$_SESSION['loggedin'] = true;
+		
+        if ($admin['Admin'] == 1) {
+            $_SESSION['Admin'] = 1;
+			
+			header ("Location:beheer.php");
+        } else  {
+            header ("Location:login_success.php");
+            $_SESSION['Admin'] = 0;
+        } 
+		
+		} else  {
+		header("refresh: 0; url=logindenied.php");
+		
+        }
+
+
+?>
+
+</font>
+</body>
+
+<?php
+
+/* maak de resultset leeg */
+mysqli_free_result($result);
+
+/* sluit de connection */
+mysqli_close($conn);
+
+include ('includes/footer.html');
+
+?>
