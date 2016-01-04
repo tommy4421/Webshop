2755f173b4e695e333df7f68d32f68cf7e332669
TheKevinator <kjneeter@avans.nl>
2016-01-04 11:14:59 +0100

    Create membersarea.php

---
+++ membersarea.php
@@ -0,0 +1,19 @@
+<?php
+//index.php
+//startscherm van de webwinkel
+// Zet het niveau van foutmeldingen zo dat warnings niet getoond worden.
+error_reporting(E_ERROR | E_PARSE);
+$page_title = 'Tijdvooreenbox.nl ~ Membersarea';
+include ('includes/header.html');
+// mysqli_connect.php bevat de inloggegevens voor de database.
+// Per server is er een apart inlogbestand - localhost vs. remote server
+//include ('includes/mysqli_connect_'.$_SERVER['SERVER_NAME'].'.php');
+include ('includes/mysqli_connect_localhost.php');
+
+?>
+
+Test
+
+<?php	
+	include ('includes/footer.html');
+?>
