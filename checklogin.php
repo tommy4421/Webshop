ae686eeabcd9a21448602744ffcc7d1fcd0c3be2
TheKevinator <kjneeter@avans.nl>
2016-01-04 17:00:56 +0100

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -75,10 +75,9 @@ $klantnr2 = mysql_fetch_array($klantnr1);
 $klantnr = ($klantnr2['KlantID']);
 
 		session_start();
-session_register("klantnr");
-session_register("naam"); 
-session_register("email"); 
-
+		$_SESSION['klantnr'] = $klantnr;
+ 		$_SESSION['Naam'] = $naam;
+ 		$_SESSION['Email'] = $email;
 		$_SESSION['loggedin'] = true;
 		header("location:login_success.php");
 	
