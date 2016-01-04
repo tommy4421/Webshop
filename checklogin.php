db5f781c270804ca7382de192c4a88b8be051620
TheKevinator <kjneeter@avans.nl>
2016-01-04 01:29:27 +0100

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -20,14 +20,20 @@ include ('includes/mysqli_connect_localhost.php');
 error_reporting(E_ERROR | E_PARSE);
 
 // Stap 1: maak verbinding met MySQL.
-mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 if (mysqli_connect_errno()) {
 	printf("<p><b>Fout: verbinding met de database mislukt.</b><br/>\n%s</p>\n", mysqli_connect_error());
 	include ('includes/footer.html');
 	exit();
 }
 
+$host = 'localhost';
+$username = 'bimivp2e4';
+$password = 'Welkom01';
+$db_name = 'avans_bimivp2e4';
 
+mysql_connect("$host", "$username", "$password")or die("Kan niet verbinden met de database!"); 
+mysql_select_db("$db_name")or die("Kan geen database selecteren!");
 
 $email=$_POST['email'];
 $wachtwoord=$_POST['wachtwoord'];
