94b14cc53f10471ab9513ebdc3db673a5dca9a95
TheKevinator <kjneeter@avans.nl>
2016-01-04 11:20:18 +0100

    Update membersarea.php

--- membersarea.php
+++ membersarea.php
@@ -12,8 +12,7 @@ include ('includes/header.html');
 include ('includes/mysqli_connect_localhost.php');
 
 session_start();
-	if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-    header("location: login.php");
+	if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {;
 } else {
     header("location: index.php");
 }
