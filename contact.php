3be4baad3d8f1ede3375e3449f321f132e8b0e1b
MimsM <mpmourik@avans.nl>
2016-01-17 10:33:37 +0100

    contactformulier toegevoegd

--- contact.php
+++ contact.php
@@ -49,46 +49,102 @@ if (mysqli_connect_errno()) {
 	exit();
 }
 
-// Opdracht: Maak de juiste SQL query die hier de informatie over onze producten gaat opleveren.
- 
-$sql = "SELECT * FROM Product"; 
+session_start(); 
+$mail_ontv = 'contact@tijdvooreenbox.nl';
 
-// Voer de query uit en sla het resultaat op 
-$result = mysqli_query($conn, $sql);
-	
-if($result === false) {
-	echo "<p>Er zijn geen producten in de winkel gevonden.</p>\n";
-} else {
-	$num = 0;
-	$num = mysqli_num_rows($result);
-	echo "<p>Er zijn ".$num." producten gevonden.</p>\n";
-}
-
-// Laat de producten zien in een form, zodat de gebruiker ze kan selecteren.
-// Haal een nieuwe regel op uit het resultaat, zolang er nog regels beschikbaar zijn.
-// We gebruiken in dit geval een associatief array.
-while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) 
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-	echo "<!-- ---------------------------------- -->\n";
-	echo "<div id=\"product\">\n<form action=\"add.php\" method=\"post\">\n";
-	echo "<input type=\"hidden\" name=\"productnummer\" value=\"".$row["productid"]."\" />\n";
-	echo "<input type=\"hidden\" name=\"prijs\" value=\"".$row["prijs"]."\" />\n";
-	echo '<p><img id=\'plaatje\' src="showfile.php?image_id='.$row["image_id"].'"></p>';
-	echo "<div id=\"prijs\">€ ".number_format($row["Prijs_Perstuk"], 2, ',', '.')."</div>\n";
-	echo "<div id=\"prodnaam\">".$row["Naam"]."</div>\n";
-	echo "<div id=\"beschrijving\">".$row["beschrijving"]."</div>\n";
-	echo "<div id=\"leverbaar\">Leverbaar: ".$row["Leverbaar"]."</div>\n";
-	echo "<div id=\"voorraad\">Voorraad: ".$row["Voorraad_aantal"]."</div>\n";
-	echo "<div id=\"selecteer\">Aantal: <input type=\"number\" name=\"hoeveelheid\" size=\"2\" maxlength=\"2\" value=\"1\" min=\"1\" max=\"".$row["voorraad"]."\"/>";
-	echo "<input type=\"submit\" value=\"Bestel\" class=\"button\"/></div>\n";
-	echo "</form>\n</div>\n";
+    if (empty($_POST['naam']))
+        $naam_fout = 1;
+    if (function_exists('filter_var') && !filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL))
+            $email_fout = 1;
+    if (!empty($_SESSION['antiflood']))
+    {
+        $seconde = 20;
+        $tijd = time() - $_SESSION['antiflood'];
+        if($tijd < $seconde)
+            $antiflood = 1;
+    }
 }
 
-/* maak de resultset leeg */
-mysqli_free_result($result);
-
-/* sluit de connection */
-mysqli_close($conn);
+if (($_SERVER['REQUEST_METHOD'] == 'POST' && (!empty($antiflood) || empty($_POST['naam']) || !empty($naam_fout) || empty($_POST['mail']) || !empty($email_fout) || empty($_POST['bericht']) || empty($_POST['onderwerp']))) || $_SERVER['REQUEST_METHOD'] == 'GET')
+{
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        if (!empty($naam_fout))
+            echo '<p>Uw naam is niet ingevuld.</p>';
+        elseif (!empty($email_fout))
+            echo '<p>Uw e-mailadres is niet juist.</p>';
+        elseif (!empty($antiflood))
+            echo '<p>U mag slechts &eacute;&eacute;n bericht per ' . $seconde . ' seconde versturen.</p>';
+        else
+            echo '<p>U bent uw naam, e-mailadres, onderwerp of bericht vergeten in te vullen.</p>';
+    }
+        
+  echo '<h1> Contactformulier</h1>
+  <form method="post" action="' . $_SERVER['REQUEST_URI'] . '" />
+  <p>
+  
+      <label for="naam">Naam:</label><br />
+      <input type="text" id="naam" name="naam" value="' . (isset($_POST['naam']) ? htmlspecialchars($_POST['naam']) : '') . '" /><br /><br />
+      
+      <label for="mail">E-mailadres:</label><br />
+      <input type="text" id="mail" name="mail" value="' . (isset($_POST['mail']) ? htmlspecialchars($_POST['mail']) : '') . '" /><br /><br />
+      
+      <label for="onderwerp">Onderwerp:</label><br />
+      <input type="text" id="onderwerp" name="onderwerp" value="' . (isset($_POST['onderwerp']) ? htmlspecialchars($_POST['onderwerp']) : '') . '" /><br /><br />
+      
+      <label for="bericht">Bericht:</label><br />
+      <textarea id="bericht" name="bericht" rows="8" style="width: 400px;">' . (isset($_POST['bericht']) ? htmlspecialchars($_POST['bericht']) : '') . '</textarea><br /><br />
+      
+      <input type="submit" name="submit" value=" Versturen " />
+  </p>
+  </form>';
+}
+else
+{      
+  $datum = date('d/m/Y H:i:s');
+    
+  $inhoud_mail = "===================================================\n";
+  $inhoud_mail .= "Ingevuld contact formulier Tijdvooreenbox.nl " . "\n";
+  $inhoud_mail .= "===================================================\n\n";
+  
+  $inhoud_mail .= "Naam: " . htmlspecialchars($_POST['naam']) . "\n";
+  $inhoud_mail .= "E-mail adres: " . htmlspecialchars($_POST['mail']) . "\n\n";
+  $inhoud_mail .= "Bericht:\n";
+  $inhoud_mail .= htmlspecialchars($_POST['bericht']) . "\n\n";
+    
+  $inhoud_mail .= "Verstuurd op " . $datum . "\n\n";
+    
+  $inhoud_mail .= "===================================================\n\n";
+ 
+  
+  $headers = 'From: ' . htmlspecialchars($_POST['naam']) . ' <' . $_POST['mail'] . '>';
+  
+  $headers = stripslashes($headers);
+  $headers = str_replace('\n', '', $headers);
+  $headers = str_replace('\r', '', $headers);
+  $headers = str_replace("\"", "\\\"", str_replace("\\", "\\\\", $headers)); 
+  
+  $_POST['onderwerp'] = str_replace('\n', '', $_POST['onderwerp']); 
+  $_POST['onderwerp'] = str_replace('\r', '', $_POST['onderwerp']); 
+  $_POST['onderwerp'] = str_replace("\"", "\\\"", str_replace("\\", "\\\\", $_POST['onderwerp']));
+  
+  if (mail($mail_ontv, $_POST['onderwerp'], $inhoud_mail, $headers))
+  {
+      $_SESSION['antiflood'] = time();
+      
+      echo '<h1>Het contactformulier is verzonden</h1>
+      
+      <p>Bedankt voor het invullen van het contactformulier. We zullen zo spoedig mogelijk contact met u opnemen.</p>';
+  }
+  else
+  {
+      echo '<h1>Het contactformulier is niet verzonden</h1>
+      
+      <p><b>Onze excuses.</b> Het contactformulier kon niet verzonden worden.</p>';
+  }
+}
 
 
 ?>
