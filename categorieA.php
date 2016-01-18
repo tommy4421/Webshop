bf96c597af5ae03fa4a33762adf4cbd152b94b1b
TheKevinator <kjneeter@avans.nl>
2016-01-18 14:07:07 +0100

    Update categorieA.php

--- categorieA.php
+++ categorieA.php
@@ -32,7 +32,8 @@ session_start();
 
 <?php
 
-echo "<h1><center>Streekpakketten</center></h1>";
+echo "<h1><center>Streekpakketten</center></h1><br />";
+echo "<h4><center>Test</center></h5><br />";
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
  
