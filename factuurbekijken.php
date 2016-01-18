32d6819bd8469e82627ace7079d7f1f8b522ba17
TheKevinator <kjneeter@avans.nl>
2016-01-18 01:24:34 +0100

    Update factuurbekijken.php

--- factuurbekijken.php
+++ factuurbekijken.php
@@ -72,25 +72,14 @@ while($row = mysqli_fetch_array($result2, MYSQLI_ASSOC))
 	echo "<div id=\"klantgebeuren\">\n<form action=\"factuurbekijken.php\" method=\"post\">\n";
 	echo "<center>Product</center><br />";
 	echo "<input type=\"hidden\" name=\"klantnr\" value=\"".$klantid."\" />\n";
-	$product = ".$row["Pro_ProductID"].";
+	echo "Productnaam: <input type=\"text\" name=\"FactuurID\" value=\"".$row["Pro_Naam"]."\" />\n";
 	echo "ProductID: <input type=\"text\" name=\"FactuurID\" value=\"".$row["Pro_ProductID"]."\" />\n";
 	echo "Aantal: <div id=\"Prijs\">&euro;".$row["Prijs"]."</div>\n<br />";
 	echo "<div id=\"postcode\">Aantal: ".$row["Aantal"]."</div>\n";
 	echo "</form>\n</div>\n";
 }
 
-$sql3 = "SELECT * FROM Product WHERE ProductID = $product";
-$result3 = mysqli_query($conn, $sql3);
 
-while($row = mysqli_fetch_array($result3, MYSQLI_ASSOC)) 
-{
-	echo "<!-- ---------------------------------- -->\n";
-	echo "<div id=\"klantgebeuren\">\n<form action=\"factuurbekijken.php\" method=\"post\">\n";
-	echo "<center>Product</center><br />";
-	echo "<input type=\"hidden\" name=\"klantnr\" value=\"".$klantid."\" />\n";
-	echo "Product naam: <input type=\"text\" name=\"FactuurID\" value=\"".$row["Naam"]."\" />\n";
-	echo "</form>\n</div>\n";
-}
 
 
 /* maak de resultset leeg */
