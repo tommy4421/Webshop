91b127f0d1decb1108fdd39f3966e8b836827590
TheKevinator <kjneeter@avans.nl>
2016-01-18 14:18:27 +0100

    Update categorieB.php

--- categorieB.php
+++ categorieB.php
@@ -12,11 +12,8 @@ $page_title = 'Welkom in de WebWinkel';
 $active = 3;	// Zorgt ervoor dat header.html weet dat dit het actieve menu-item is.
 include ('includes/header.html');
 
-echo "<h1>Producten van categorie B</h1>";
 echo "<h1><center>Streekpakketten</center></h1>";
-echo "<h4><center>Selecteer een provincie</center></h4><br />";
 
-echo "<h4><center>Groningen</center></h4>";
 
 include ('includes/footer.html');
 ?>
