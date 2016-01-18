fbaaa0e4c9b70a0752b3f93b1a550916448a00d6
TheKevinator <kjneeter@avans.nl>
2016-01-18 01:55:43 +0100

    Update orderwijzigen.php

--- orderwijzigen.php
+++ orderwijzigen.php
@@ -67,6 +67,7 @@ while($row = mysqli_fetch_array($result2, MYSQLI_ASSOC))
 	echo "<input type=\"hidden\" name=\"klantnr\" value=\"".$klantid."\" />\n";
 	echo "OrderID: <input type=\"text\" name=\"klantnr\" value=\"".$row["Ord_orderID"]."\" />\n<br />";
 	echo "ProductID: <input type=\"text\" name=\"klantnr\" value=\"".$row["Pro_ProductID"]."\" />\n";
+	echo "Productnaam: <input type=\"text\" name=\"klantnr\" value=\"".$row["Pro_Naam"]."\" />\n";
 	echo "Prijs: <div id=\"Prijs\">&euro;".$row["Product_prijs"]."</div>\n<br />";
 	echo "<div id=\"postcode\">Aantal: ".$row["Aantal"]."</div>\n";
 	echo "</form>\n</div>\n";
