002d1c7ec45cb10bdb18bb880f12f7f8fb6ed1ab
TheKevinator <kjneeter@avans.nl>
2016-01-18 14:16:55 +0100

    Update categorieB.php

--- categorieB.php
+++ categorieB.php
@@ -15,7 +15,7 @@ include ('includes/header.html');
 echo "<h1>Producten van categorie B</h1>";
 echo "<h1><center>Streekpakketten</center></h1>";
 echo "<h4><center>Selecteer een provincie</center></h4><br />";
-echo "<h4><center><a href=friesland.php>Friesland</a></center></h4>";
+echo "<a href=\"friesland.php\">Friesland</a>";
 echo "<h4><center>Groningen</center></h4>";
 
 include ('includes/footer.html');
