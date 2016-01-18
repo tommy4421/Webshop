5d9e9e647c4061184fa0327446afbec60b3ee85a
TheKevinator <kjneeter@avans.nl>
2016-01-18 01:11:19 +0100

    Update factuurbekijken.php

--- factuurbekijken.php
+++ factuurbekijken.php
@@ -71,6 +71,7 @@ while($row = mysqli_fetch_array($result2, MYSQLI_ASSOC))
 	echo "<div id=\"klantgebeuren\">\n<form action=\"factuurbekijken.php\" method=\"post\">\n";
 	echo "<center>Product</center><br />";
 	echo "<input type=\"hidden\" name=\"klantnr\" value=\"".$klantid."\" />\n";
+	echo "Product naam: VARIABELE NAAM HIER";
 	echo "ProductID: <input type=\"text\" name=\"FactuurID\" value=\"".$row["Pro_ProductID"]."\" />\n";
 	echo "Aantal: <div id=\"Prijs\">&euro;".$row["Prijs"]."</div>\n<br />";
 	echo "<div id=\"postcode\">Aantal: ".$row["Aantal"]."</div>\n";
