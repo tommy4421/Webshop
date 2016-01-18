f21d88a2b3a103bac1783f682905fdeadddb2073
TheKevinator <kjneeter@avans.nl>
2016-01-18 12:13:14 +0100

    Update productbeheer2.php

--- productbeheer2.php
+++ productbeheer2.php
@@ -66,7 +66,7 @@ $prijs = ($prijsinstelling2['Prijs_Perstuk']);
         <ol>
           <li>
             <label for="naam">Naam</label>
-            <input id="naam" name="productid" type="hidden "value="<?php echo "$idi"; ?>" REQUIRED/>
+            <input id="naam" name="productid" type="hidden" "value="<?php echo "$idi"; ?>" REQUIRED/>
           </li>
           <li>
             <label for="naam">Naam</label>
