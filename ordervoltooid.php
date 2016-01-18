910ca829e56bc6f475724d7c95b95da70a94ca67
TheKevinator <kjneeter@avans.nl>
2016-01-18 02:02:31 +0100

    Create ordervoltooid.php

---
+++ ordervoltooid.php
@@ -0,0 +1,30 @@
+<?php
+//index.php
+//startscherm van de webwinkel
+// Zet het niveau van foutmeldingen zo dat warnings niet getoond worden.
+error_reporting(E_ERROR | E_PARSE);
+$page_title = 'Tijdvooreenbox.nl ~ Login';
+include ('includes/header.html');
+// mysqli_connect.php bevat de inloggegevens voor de database.
+// Per server is er een apart inlogbestand - localhost vs. remote server
+//include ('includes/mysqli_connect_'.$_SERVER['SERVER_NAME'].'.php');
+include ('includes/mysqli_connect_localhost.php');
+
+session_start();
+    if(!$_SESSION["Admin"] == 1){
+     header("location: index.php");
+    exit;
+    }
+
+
+?>
+    <center>
+
+Test
+
+     </center>
+
+<?php	
+} 	echo "<br />";
+	include ('includes/footer.html');
+?>
