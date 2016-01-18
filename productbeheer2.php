2bf9a1824acc25449ad6b6fb3d4c8c073296ad6a
TheKevinator <kjneeter@avans.nl>
2016-01-18 11:59:52 +0100

    Update productbeheer2.php

--- productbeheer2.php
+++ productbeheer2.php
@@ -23,14 +23,36 @@ session_start();
  
  <?php
 
-$ProductID = $_POST['productid'];
+$idi = $_POST['productid'];
 $gebruikersnaam = "bimivp2e4";
 $wachtwoord = "Welkom01";
 $host = "localhost";
 $database = "avans_bimivp2e4";
+$tabel = "Product";
+
 
 mysql_connect("$host", "$gebruikersnaam", "$wachtwoord")or die("Het is niet gelukt om te verbinden met MYSQL!");
 mysql_select_db("$database")or die("De database kan niet worden geselecteerd!");
+
+$naaminstelling1 = mysql_query("SELECT Naam FROM $tabel WHERE ProductID='$idi'");
+$naaminstelling2 = mysql_fetch_array($naaminstelling1);
+$naam = ($naaminstelling2['Naam']);
+
+$beschrijvinginstelling1 = mysql_query("SELECT Beschrijving FROM $tabel WHERE ProductID='$idi'");
+$beschrijvinginstelling2 = mysql_fetch_array($beschrijvinginstelling1);
+$beschrijving = ($beschrijvinginstelling2['Beschrijving']);
+
+$provincieinstelling1 = mysql_query("SELECT Provincie FROM $tabel WHERE ProductID='$idi'");
+$provincieinstelling2 = mysql_fetch_array($provincieinstelling1);
+$provincie = ($provincieinstelling2['Provincie']);
+
+$voorraadinstelling1 = mysql_query("SELECT Voorraad_aantal FROM $tabel WHERE ProductID='$idi'");
+$voorraadinstelling2 = mysql_fetch_array($voorraadinstelling1);
+$voorraad = ($voorraadinstelling2['Voorraad_aantal']);
+
+$prijsinstelling1 = mysql_query("SELECT Prijs_Perstuk FROM $tabel WHERE ProductID='$idi'");
+$prijsinstelling2 = mysql_fetch_array($prijsinstelling1);
+$prijs = ($prijsinstelling2['Prijs_Perstuk']);
 
 ?>
  
@@ -44,35 +66,35 @@ mysql_select_db("$database")or die("De database kan niet worden geselecteerd!");
         <ol>
           <li>
             <label for="naam">Naam</label>
-            <input id="naam" name="naam" value="" REQUIRED/>
+            <input id="naam" name="naam" value="<?php echo "$naam"; ?>" REQUIRED/>
           </li>
 		  <li>
             <label for="naam">Beschrijving</label>
-	    <textarea rows="4" cols="50" name="beschrijving" REQUIRED></textarea>
+	    <textarea rows="4" cols="50" name="beschrijving" value="<?php echo "$beschrijving"; ?> REQUIRED></textarea>
           </li>
 		  <li>
             <label for="naam">Provincie</label>
-            <input id="provincie" name="provincie" REQUIRED/>
+            <input id="provincie" name="provincie" value="<?php echo "$provincie"; ?> REQUIRED/>
           </li>
 		  <li>
             <label for="naam">Voorraad aantal</label>
-            <input id="naam" name="voorraad" REQUIRED/>
+            <input id="naam" name="voorraad" value="<?php echo "$voorraad"; ?> REQUIRED/>
           </li>
            <li>
             <label for="naam">Prijs per stuk</label>
-            <input id="naam" name="prijs" REQUIRED/>
+            <input id="naam" name="prijs" value="<?php echo "$prijs"; ?> REQUIRED/>
             </li>
             <li>
             <label for="leverbaar">Leverbaar<em>*</em></label><br />
           <label>
-            <input type="radio" name="leverbaar" value="Ja" checked="checked" REQUIRED/>
+            <input type="radio" name="leverbaar" value="Ja" checked="checked"/>
             Ja</label>
           <label>
             <input type="radio" name="leverbaar" value="Nee"/>
             Nee</label>
           </li>
         </ol>
-        <input type="submit" value="Voeg product toe" class="button"/>
+        <input type="submit" value="Wijzig product" class="button"/>
       </fieldset>
     </form>
 	
@@ -105,8 +127,7 @@ mysql_select_db("$database")or die("De database kan niet worden geselecteerd!");
 				if ($conn->connect_error) {
 				die("Kan geen verbinding maken: " . $conn->connect_error);
 			} 
-				$sql = "INSERT INTO $tabel (Naam, Beschrijving, Provincie, Voorraad_aantal, Prijs_Perstuk, Leverbaar)
-				VALUES ('$naam', '$beschrijving', '$provincie', '$voorraad', '$prijs', '$leverbaar')";
+				$sql = "UPDATE $tabel SET Naam='$naam', Beschrijving='$beschrijving', Provincie='$provincie', Voorrraad='$voorraad', Prijs='$prijs' Leverbaar='$leverbaar' WHERE ProductID='$idi'";
 
 					if ($conn->query($sql) === TRUE) {
 					echo "Product succesvol gewijzigd!";
@@ -122,12 +143,6 @@ mysql_select_db("$database")or die("De database kan niet worden geselecteerd!");
 	}
 	}
 
-	<form action="productverwijderen.php" method="POST" class="formulier">
-		<ul>
-            <input id="productid" name="productid" type="hidden" value="<?php echo "$ProductID"; ?>" />
-        </ul>
-		<input type="submit" value="Verwijder product" class="button"/>
-	</form><br />
 
 
 
@@ -136,6 +151,14 @@ mysql_select_db("$database")or die("De database kan niet worden geselecteerd!");
 
 	?>
 
+		<form action="productverwijderen.php" method="POST" class="formulier">
+		<ul>
+            <input id="productid" name="productid" type="hidden" value="<?php echo "$ProductID"; ?>" />
+        </ul>
+		<input type="submit" value="Verwijder product" class="button"/>
+	</form><br />
+	
+	
 <?php	
 	
 	include ('includes/footer.html');
